2c2d5ddab6f14d7a54e6e129ef8de15912b9c047
Windo Tri Ananda Syaputra <windotriananda@gmail.com>
2022-12-13 13:35:57 +0700

    Initial commit

---
+++ src/test/java/com/windo/pengembalian/PengembalianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windo.pengembalian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengembalianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
